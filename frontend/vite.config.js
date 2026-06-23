fd2bba0c4e21200393161967334877dbc9b40fa6
root <root@srv1214085.hstgr.cloud>
2026-06-23 02:06:36 +0000

    temp: vps

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,11 +10,11 @@ export default defineConfig({
         },
     },
     server: {
-        port: 3000,
+        port: 5000,
         strictPort: true,
     },
     preview: {
-        port: 3000,
+        port: 5000,
         strictPort: true,
     },
 });
